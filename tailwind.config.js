1bc378f3fa89ab59b482c915c74cc4ae95fe4d41
eaysin-arafat <eaysin.arafat.j@gmail.com>
2024-12-22 03:35:54 +0600

    fix: replace tailwind config file for deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -84,5 +86,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
